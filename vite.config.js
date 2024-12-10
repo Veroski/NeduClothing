ba7d7c8c78de6a59e2e6369f69e60581af594d19
Veroski <guillemvera@gmail.com>
2024-12-10 12:35:12 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NeduClothing/', // Reemplaza 'NeduClothing' con el nombre exacto de tu repositorio
+  base: '/', // Para desarrollo local sin especificar carpeta base
 });
